b122042b20e9fd35b616effe7ba43fd3b9923666
Marco Romanutti <m.romanutti@gmail.com>
2022-08-31 20:40:53 +0200

    Add zuehlke logo element

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import 'reveal.js/dist/reveal.css';
 import './styles/fonts/googlecode.css';
-import './styles/zuehlke.css';
 import './styles/slides/zuehlke-logo-slide.css';
 import './styles/slides/zuehlke-title-slide.css';
 import './styles/slides/zuehlke-subtitle-slide.css';
 import './styles/slides/zuehlke-agenda-slide.css';
 import './styles/slides/zuehlke-exercise-slide.css';
 import './styles/slides/zuehlke-break-slide.css';
+import './styles/zuehlke.css';
+import './styles/elements/zuehlke-logo.css';
 import './styles/zuehlke-print.css';
 
 import Reveal from "reveal.js";
